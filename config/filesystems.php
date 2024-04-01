cfa904dc958699f0d848fb55a43abb6cccaa977e
saeed <saeed@corals.io>
2024-04-02 01:17:33 +0300

    fix: update media disk default visibility

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,6 +66,17 @@ return [
             'driver' => 'local',
             'root' => public_path() . '/media',
             'url' => '/media',
+            'visibility' => 'public',
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
         ],
         'secure_media' => [
             'driver' => 'local',
