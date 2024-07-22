ca891de849101b7357286a291b9a536a54af3380
Adam Saleem <adamsaleem2001.01@gmail.com>
2024-07-22 14:45:30 +0300

    chore: new structure for migrations file

--- Corals/core/Settings/database/migrations/2017_12_31_000000_create_modules_table.php
+++ Corals/core/Settings/database/migrations/2017_12_31_000000_create_modules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateModulesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -44,4 +44,4 @@ class CreateModulesTable extends Migration
     {
         Schema::dropIfExists('modules');
     }
-}
+};
